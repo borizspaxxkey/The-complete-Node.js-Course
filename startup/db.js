19ccf3b2dd7bd77e84966e771e9885eef5b980dd
JosephCollins Udeani <JosephCollins.Udeani@aa.com>
2020-05-29 18:36:00 -0500

    changed to config for db

--- startup/db.js
+++ startup/db.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose');
 const config = require('config');
 
 module.exports = function () {
-  mongoose.connect('mongodb+srv://spaxxkey:91408916B.c@cluster0-sc4m5.mongodb.net/test?retryWrites=true&w=majority')
+  mongoose.connect(config.get('db'))
     .then(() => winston.info('Connected to MongoDB...'))
 }
